5eb2ed49390a2cb95572cd22d8b5f95021036392
Elvis Chege <elvischege98@gmail.com>
2026-05-18 13:17:04 +0300

    Created my three home,AddBook and AvaiableBooks components

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,9 @@
+function AddBook(){
+return(
+    <>
+    <h1>Add book</h1>
+    </>
+)
+}
+
+export default AddBook; 
---
+++ src/components/AvailableBooks.jsx
@@ -0,0 +1,9 @@
+function AvailableBooks(){
+    return(
+        <>
+        <h1>Available Books</h1>
+        </>
+    )
+}
+
+export default AvailableBooks;
