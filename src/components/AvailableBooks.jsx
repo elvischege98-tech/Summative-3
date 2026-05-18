6ddbd089f1ae7759d5556af4e5a99036205ebd55
Elvis Chege <elvischege98@gmail.com>
2026-05-18 14:06:51 +0300

    Inline styled h1 in AvailableBooks component

--- src/components/AvailableBooks.jsx
+++ src/components/AvailableBooks.jsx
@@ -1,7 +1,15 @@
 function AvailableBooks(){
     return(
         <>
-        <h1>Available Books</h1>
+        <h1 
+        style={{
+            textAlign: 'center',
+            marginTop: '50px',
+            color: '#3b2f1f',
+            fontSize: '2.5rem',
+            fontWeight: 'bold',
+            fontFamily: 'Georgia, serif',
+        }}>Available Books</h1>
         </>
     )
 }
