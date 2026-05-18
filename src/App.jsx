36d2af9c24261e6c87f3057e7db09a8c5af4e492
Elvis Chege <elvischege98@gmail.com>
2026-05-18 19:10:09 +0300

    Finished giving the main page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
-import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import Home from "./components/Home";
 import AddBook from "./components/AddBook";
 import AvailableBooks from "./components/AvailableBooks";
-import './App.css';
+
+import "./App.css";
+
+import { BooksContext } from "./context/BooksContext";
+import { useBooksAPI } from "./hooks/useBooksAPI";
 
 function App() {
-  return (
-    <>
-       <BrowserRouter >
-       <div className="app-wrapper">
-            <nav className="nav">
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/add-book">Add Book</Link></li>
-                <li><Link to="/available-books">Available Books</Link></li>
-              </ul>
-            </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add-book" element={<AddBook />} />
-          <Route path="/available-books" element={<AvailableBooks />} />
-        </Routes>
-        </div>
-      </BrowserRouter>
-      
-    </>
-  );
+    const booksData = useBooksAPI();
+
+    return (
+        <BooksContext.Provider value={booksData}>
+            <BrowserRouter>
+                <div className="app-wrapper">
+
+                    <nav className="nav">
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/add-book">Add Book</Link></li>
+                            <li><Link to="/available-books">Available Books</Link></li>
+                        </ul>
+                    </nav>
+
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/add-book" element={<AddBook />} />
+                        <Route path="/available-books" element={<AvailableBooks />} />
+                    </Routes> 
+
+                    </div>
+
+            </BrowserRouter>
+        </BooksContext.Provider>
+    );
 }
+
 export default App;
+
+
 
